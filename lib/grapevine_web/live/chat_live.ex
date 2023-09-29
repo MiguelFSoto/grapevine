ed1bb90d4fddbf7c3c09bcaec3461145da0b6491
miguelfsoto <miguel@soto.net.co>
2023-09-29 15:03:32 -0500

    Add basic chat functionality

--- lib/grapevine_web/live/chat_live.ex
+++ lib/grapevine_web/live/chat_live.ex
@@ -2,6 +2,15 @@ defmodule GrapevineWeb.ChatLive do
   use GrapevineWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    defParams = %{messages:  [""], msg: ""}
+    {:ok, assign(socket, defParams)}
+    # put_resp_cookie(socket, "messages", [], sign: true)
+  end
+
+  def handle_event("send_message", %{"messages" => exMessages, "msg" => msgSent}, socket) do
+    # %{"messages" => exMessages, "msg" => msgSent} = params
+    fullMsgList = [msgSent | exMessages]
+    nextParams = %{ messages: fullMsgList, msg: "" }
+    {:noreply, assign(socket, nextParams)}
   end
 end
