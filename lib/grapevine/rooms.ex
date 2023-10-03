124bc14c9dac13733630ad466a567ad3ca4626b6
miguelfsoto <miguel@soto.net.co>
2023-10-03 11:14:14 -0500

    Add initial room context functions

---
+++ lib/grapevine/rooms.ex
@@ -0,0 +1,35 @@
+defmodule Grapevine.Rooms do
+  @moduledoc """
+  The Message context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Grapevine.Repo
+
+  alias Grapevine.Rooms.Room
+
+  @doc """
+  Returns a list of rooms that a given user is part of
+  """
+  def user_rooms(mail) do
+    query = from(Room, where:[Enum.member?(:members, mail)], select: [:name])
+    Repo.all(query)
+  end
+
+  @doc """
+  Returns a list of the users in a given room
+  """
+  def room_users(room) do
+    query = from(Room, where:[name: room], select: [:members])
+    Repo.all(query)
+  end
+
+  @doc """
+  Saves a sent message
+  """
+  def create_room(attrs) do
+    %Message{}
+    |> Message.changeset(attrs)
+    |> Repo.insert()
+  end
+end
